6f050b56c0d4e44faaab8e77fc43403bd2b708f8
Guy Moreno <173142004+GuyMoreno@users.noreply.github.coml173142004+GuyMoreno@users.noreply.github.com>
2026-04-22 13:41:03 +0300

    added nav linkes hover effect

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@
 
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
-
+import "../src/index.css";
 
 // App is always shown
-  // Nav is PERMANENT
-  // Outlet is varing... showing different content 
+// Nav is PERMANENT
+// Outlet is varing... showing different content
 const App = () => {
   return (
     <div>
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
+import "../src/index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
