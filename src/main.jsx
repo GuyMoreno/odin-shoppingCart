d0895f08bac9382127488ebbd91a6b04ac950d0e
Guy Moreno <173142004+GuyMoreno@users.noreply.github.coml173142004+GuyMoreno@users.noreply.github.com>
2026-04-23 09:36:52 +0300

    added cart functionality

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,12 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
 import "../src/index.css";
+import { CartProvider } from "./CartContext"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
+    <CartProvider>
+      <RouterProvider router={router} />
+    </CartProvider>
+  </StrictMode>
 );
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -2,6 +2,7 @@
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import "./Shop.css";
+import { useCart } from "../CartContext"
 
 // We import React tools:
 // useState = store data that can change
@@ -14,12 +15,13 @@ const Shop = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const { cart, setCart } = useCart();
   
   useEffect(() => {
     const fetchProducts = async () => {
       try {
         const res = await fetch(
-          "https://fakestoreapi.com/products?limit=3"
+          "https://fakestoreapi.com/products?limit=7"
         );
 
         const data = await res.json();
@@ -36,6 +38,21 @@ const Shop = () => {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>{error}</p>;
 
+  // Cart
+  const addToCart = (product) => {
+  setCart((prev) => {
+    const existing = prev.find((item) => item.id === product.id);
+    if (existing) {
+      return prev.map((item) =>
+        item.id === product.id
+          ? { ...item, quantity: item.quantity + 1 }
+          : item
+      );
+    }
+    return [...prev, { ...product, quantity: 1 }];
+  });
+};
+
   return (
     <div>
       <h1>Shop</h1>
@@ -44,6 +61,7 @@ const Shop = () => {
         <div className="product-card" key={product.id}>
           <h3>{product.title}</h3>
           <p>${product.price}</p>
+          <button onClick={() => addToCart(product)}>Add to Cart</button>
         </div>
       ))}
       </div>
