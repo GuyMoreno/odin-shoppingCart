badfd22b93a280d0fc0e0a9db0d0160228d30fbf
Guy Moreno <173142004+GuyMoreno@users.noreply.github.coml173142004+GuyMoreno@users.noreply.github.com>
2026-04-23 11:33:31 +0300

    added cart functiolitty and style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,14 @@
 // Navbar.jsx
 import { Link } from "react-router-dom";
 import "../Navbar.css";
+import { useCart } from "../CartContext";
 
 const Navbar = () => {
+  const { cart } = useCart();
+
+  const totalItems = cart.reduce((sum, item) => {
+  return sum + item.quantity;
+}, 0);
   return (
     <nav>
       <ul>
@@ -13,8 +19,12 @@ const Navbar = () => {
           <Link to="/shop">Shop</Link>
         </li>
         <li>
-          <Link to="/cart">Cart</Link>
-        </li>
+
+  <Link to="/cart">
+    Cart ({totalItems})
+  </Link>
+
+</li>
       </ul>
     </nav>
   );
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,12 +1,66 @@
 // Cart.jsx
 import { Link } from "react-router-dom";
+import { useCart } from "../CartContext";
+import "../Cart.css";
 
 const Cart = () => {
+  const { cart, setCart } = useCart();
+
+  // Plus
+  const increase = (id) =>
+    setCart((prev) =>
+      prev.map((item) =>
+        item.id === id
+          ? { ...item, quantity: item.quantity + 1 }
+          : item
+      )
+    );
+  // Minus
+  const decrease = (id) =>
+    setCart((prev) =>
+      prev.map((item) =>
+          item.id === id
+            ? { ...item, quantity: item.quantity - 1 }
+            : item
+        )
+        .filter((item) => item.quantity > 0)
+    );
+  
+  
   return (
-    <>
-      <p>Cart</p>
-      <Link to="/">Click here to go back</Link>
-    </>
+    <div className="cart">
+
+    <h1 className="cart-title">Cart</h1>
+
+    {cart.length === 0 && <p>Your cart is empty</p>}
+
+    <div className="cart-list">
+
+      {cart.map((item) => (
+
+        <div className="cart-item" key={item.id}>
+
+          <h3>{item.title}</h3>
+
+          <p>Price: ${item.price}</p>
+
+          <p>Quantity: {item.quantity}</p>
+
+          <div className="cart-actions">
+
+            <button onClick={() => increase(item.id)}>+</button>
+
+            <button onClick={() => decrease(item.id)}>-</button>
+
+          </div>
+
+        </div>
+
+      ))}
+
+    </div>
+
+  </div>
   );
 };
 
