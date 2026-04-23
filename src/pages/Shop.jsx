dee499e61e39a43c84d3c2c630fc6adcab27ff3c
Guy Moreno <173142004+GuyMoreno@users.noreply.github.coml173142004+GuyMoreno@users.noreply.github.com>
2026-04-23 08:49:23 +0300

    modified api request + added grid style for products

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,34 +1,52 @@
 // Shop.jsx
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
+import "./Shop.css";
+
 // We import React tools:
 // useState = store data that can change
 // useEffect = run code when the page loads
+
 const Shop = () => {
+  
   // Create a state to store products
   // starts as an empty list
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  
   useEffect(() => {
-    // This runs only once when the page loads
-    fetch("https://fakestoreapi.com/products?limit=3")
-      // Ask the internet for 3 products
-      .then((res) => res.json())
-      // Convert the response into usable JavaScript data
-      .then((data) => setProducts(data));
-    // Save the products into state
-    // This will make the page re-render
+    const fetchProducts = async () => {
+      try {
+        const res = await fetch(
+          "https://fakestoreapi.com/products?limit=3"
+        );
+
+        const data = await res.json();
+        setProducts(data);
+      } catch (err) {
+        setError("Something went wrong");
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchProducts();
   }, []);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
 
   return (
     <div>
       <h1>Shop</h1>
-      {/* Loop through all products and show them */}
-      {products.map((product) => (
-        <div key={product.id}>
+      <div className="shop-grid">
+        {products.map((product) => (
+        <div className="product-card" key={product.id}>
           <h3>{product.title}</h3>
           <p>${product.price}</p>
         </div>
       ))}
+      </div>
     </div>
   );
 };
