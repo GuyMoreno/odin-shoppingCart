be59d1ef4cc95db18413b05bd6542c1e91e0f50e
Guy Moreno <173142004+GuyMoreno@users.noreply.github.coml173142004+GuyMoreno@users.noreply.github.com>
2026-04-27 12:08:17 +0300

    added changes to tests

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -3,6 +3,7 @@ import { render, screen } from "@testing-library/react";
 import App from "../App";
 import { CartProvider } from "../CartContext";
 import { MemoryRouter } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
 
 const renderApp = () => {
   render(
@@ -14,21 +15,29 @@ const renderApp = () => {
   );
 };
 
+// Describe = Test Suit
 describe("App basic UI", () => {
-
+  // It = a test case
   it("renders navbar", () => {
+    // Runs the App component
     renderApp();
     expect(screen.getByRole("navigation")).toBeInTheDocument();
   });
-
+  
+  // It = a test case
   it("renders shop link", () => {
     renderApp();
+    //  using regex with the i flag allows simpler case-insensitive comparison
+      // i
     expect(screen.getByText(/shop/i)).toBeInTheDocument();
   });
-
+  
+  // It = a test case
   it("renders cart link", () => {
     renderApp();
     expect(screen.getByText(/cart/i)).toBeInTheDocument();
   });
+ 
 
 });
+
